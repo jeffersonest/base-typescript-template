31f0ee77456dce00b7971b9e311383720b7257cd
jeffersonest <jesbmail@gmail.com>
2023-12-26 22:40:03 -0300

    delete: removed dist file

--- dist/server.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("reflect-metadata");
-require("./config/dependency-injection.config");
-const tsyringe_1 = require("tsyringe");
-const server = tsyringe_1.container.resolve('Main');
-server.start();
